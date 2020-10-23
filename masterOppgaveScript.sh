369d77afd2f278bfc7099eafd2d688067e2c8554
MartinJrgsn <240368@student.usn.no>
2020-10-23 10:07:22 +0200

    Added masterOppgaveScript.sh
    Added masterOppgaveScript.sh

---
+++ masterOppgaveScript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Starting masterscript to complete all tasks with one script"
+
+~/git/oblig2/oppgave1
+
+~/git/oblig2/oppgave2
+
+~/git/oblig2/oppgave3
+
+~/git/oblig2/oppgave4
+
+~/git/oblig2/oppgave5
+
+~/git/oblig2/oppgave6
